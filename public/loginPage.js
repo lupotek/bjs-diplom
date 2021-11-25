f26005a4cf52c7f548e88ecefe067a9114eb7c73
Dinara Gaynutdinova <dinarakiss84@gmail.com>
2021-11-25 22:54:44 +0300

    homepage исправлено

--- public/loginPage.js
+++ public/loginPage.js
@@ -5,7 +5,7 @@ const userForm = new UserForm ();
 userForm.loginFormCallback = data => {
     ApiConnector.login(data, (response) => {
         console.log (response);
-        if (response) {
+        if (response.success) {
             location.reload();
         } else {
             userForm.setLoginErrorMessage(response.error)
@@ -16,7 +16,7 @@ userForm.loginFormCallback = data => {
 userForm.registerFormCallback = data => {
     ApiConnector.register(data, (response) => {
         console.log(response)
-        if (response) {
+        if (response.success) {
             location.reload();
         } else {
             userForm.setRegisterErrorMessage(response.error)
